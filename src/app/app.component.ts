f786dd99968c431c6f2618be007dc8d32563bf52
ian otieno <ianotieno23@gmail.com>
2023-09-24 16:43:51 +0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+interface Person{
+  id:number,
+  name:string,
+  sex:string,
+  isCool:boolean
+}
 
 @Component({
   selector: 'app-root',
@@ -8,11 +14,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
  title:string ="Cute Beaver"
+
  imgScr:string="https://img.freepik.com/premium-vector/cartoon-happy-beaver-white-background_194935-62.jpg?w=740"
   constructor(){}
   favouriteAnimal:string =" "
   beavername:string=""
+
   handleClick(value:any){
     console.log(value)
   }
+
+persons:Person[]=[{
+  id:1,
+  name:"hellen",
+  sex:"female",
+  isCool: true
+},
+{
+  id:2,
+  name:"peter",
+  sex:"male",
+  isCool: false
+},
+{
+  id:3,
+  name:"mary",
+  sex:"female",
+  isCool: true
+},
+{
+  id:4,
+  name:"kevin",
+  sex:"male",
+  isCool: false
+},]
+
 }
