c8ea2aad2e798d0cfdcb71018e621e6ef37a48da
ian otieno <ianotieno23@gmail.com>
2023-09-24 19:55:16 +0300

    update

--- src/app/components/employee-details/employee-details.component.ts
+++ src/app/components/employee-details/employee-details.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+interface Employee{
+  id:number,
+  name:string,
+  age:number
+}
 @Component({
   selector: 'app-employee-details',
   templateUrl: './employee-details.component.html',
   styleUrls: ['./employee-details.component.css']
 })
-export class EmployeeDetailsComponent {
-
+export class EmployeeDetailsComponent implements OnInit{
+  
+  employees:Employee[]=[
+    {
+    id:1,
+    name:"tom",
+    age:20
+  },
+  {
+    id:2,
+    name:"mary",
+    age:30
+  },
+  {
+    id:4,
+    name:"paul",
+    age:22
+  }]
+  ngOnInit(){
+    
+  }
 }
