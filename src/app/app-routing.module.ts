620dc51f1cc6c82708c4dc9d80046aaeb47eb1da
ian otieno <ianotieno23@gmail.com>
2023-09-24 21:11:30 +0300

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeComponent } from './components/employee/employee.component';
+import { EmployeeDetailsComponent } from './components/employee-details/employee-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'/',component:EmployeeComponent},
+  {path:'employee details', component:EmployeeDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
