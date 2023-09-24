f39f68ff79f587e79b0847d1bd760bc2124ac4b9
ian otieno <ianotieno23@gmail.com>
2023-09-24 16:18:09 +0300

    update

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+ title:string ="Cute Beaver"
+ imgScr:string="https://img.freepik.com/premium-vector/cartoon-happy-beaver-white-background_194935-62.jpg?w=740"
+  constructor(){}
+  favouriteAnimal:string =" "
+}
